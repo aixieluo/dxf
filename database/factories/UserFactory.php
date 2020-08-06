28c4162eabe0cc31d43e77b00ad8c10cb436e9f5
xuxiaoyang <xuxiaoyang@app-ark.com>
2020-08-06 14:59:52 +0800

    fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,8 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Order;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
@@ -22,5 +24,16 @@ $factory->define(User::class, function (Faker $faker) {
         'nickname' => $faker->name,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'position_id' => random_int(1, 4)
+    ];
+});
+
+$factory->define(Order::class, function (Faker $faker) {
+    return [
+        'oid' => random_int(2, 2000000),
+        'recipient_information' => '123',
+        'note' => '123',
+        'sofa_cover_id' => 1,
+        'sofa_cover_item_id' => 1,
+        'user_id' => 2,
     ];
 });
